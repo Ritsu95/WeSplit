cd6141861676242708b4c55590c735d730e82b6d
Ritsu95 <ritsu95@icloud.com>
2019-12-01 13:13:20 +0100

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ritsu on 01/12/2019.
+//  Copyright © 2019 Ritsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
